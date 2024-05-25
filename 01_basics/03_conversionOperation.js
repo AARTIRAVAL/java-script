19b6e47725b3fcd189f46f812d2b6c63eddde95c
AARTIRAVAL <ravalarati572@gmail.com>
2024-05-25 21:27:15 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+//let score = 33 // number type
+//let score = "33" // string type
+
+//console.log(typeof score); // one tyep
+//console.log(typeof(score)); // second type
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+//let isLoggedIn = "Aarti"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Aarti" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
