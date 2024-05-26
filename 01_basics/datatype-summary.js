eb051c87fae888e34ac7af8489afe35e5388c177
AARTIRAVAL <ravalarati572@gmail.com>
2024-05-26 21:10:22 +0000

    memory

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -32,9 +32,35 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof bigNumber);
-console.log(typeof outsideTemp);
-console.log(typeof myFunction);
-console.log(typeof scoreValue);
-console.log(typeof heros);
-console.log(typeof anotherId);
+//console.log(typeof bigNumber);
+//console.log(typeof outsideTemp);
+//console.log(typeof myFunction);
+//console.log(typeof scoreValue);
+//console.log(typeof heros);
+//console.log(typeof anotherId);
+
+
+// *********************************************
+
+// stack Memory (Primitive), and Heap Memory (Non-Primitive)
+
+let myname = "AartiRaval"
+
+let anothername = myname
+anothername = "ravalaarti"
+
+//console.log(anothername);
+//console.log(myname);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "aarti@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
