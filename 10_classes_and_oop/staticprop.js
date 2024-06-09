35655f6efe82396c988c3a79a1eb45729670b92f
AARTIRAVAL <ravalarati572@gmail.com>
2024-06-09 11:58:02 +0000

    statisProperty

---
+++ 10_classes_and_oop/staticprop.js
@@ -0,0 +1,24 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+   static createID(){
+        return `123`
+    }
+}
+const Aarti = new User("Aarti")
+//console.log(Aarti.createID())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+iphone.logMe();
